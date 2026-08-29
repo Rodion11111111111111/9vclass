9b63157ea997dc7d904045624d16e0a97e7d2416
Rodion <spitakastana@gmail.com>
2026-08-29 10:26:39 +0000

    Move welcome clock one hour forward

--- welcome.js
+++ welcome.js
@@ -4,7 +4,7 @@ const welcomeDatetime = document.querySelector('#welcomeDatetime');
 function updateWelcomeDatetime() {
   welcomeDatetime.textContent = new Intl.DateTimeFormat('ru-RU', {
     day: '2-digit', month: 'long', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit'
-  }).format(new Date(Date.now() - 60 * 60 * 1000));
+  }).format(new Date(Date.now() + 60 * 60 * 1000));
 }
 
 updateWelcomeDatetime();
